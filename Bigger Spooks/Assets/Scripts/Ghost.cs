57dab467ade5ccb1d45173b298c7318bd867a612
kevponce4 <kev.ponce4@gmail.com>
2020-12-28 00:07:13 -0800

    Revert "add depossession and separate ghost form"
    This reverts commit d1fe118233d2d42c13a44babe2842f38773a5c4f.

--- Bigger Spooks/Assets/Scripts/Ghost.cs
+++ Bigger Spooks/Assets/Scripts/Ghost.cs
@@ -8,31 +8,16 @@ public class Ghost : MonoBehaviour
     private float x_input;
     private float y_input;
     private GameManager gm;
-    private SpriteRenderer ghostSR;
-    private Vector3 mousepos;
     void Start()
     {
         gm = FindObjectOfType<GameManager>();
-        ghostSR = GetComponent<SpriteRenderer>();
-        this.ghostSR.enabled = true;
-        currFurniture = null;
     }
 
     void Update()
     {
         x_input = Input.GetAxisRaw("Horizontal");
         y_input = Input.GetAxisRaw("Vertical");
-        mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        mousepos.z = 10;
-        //transform.position = mousepos;
-        this.transform.position = mousepos;
         move();
-        if (Input.GetKeyDown("z"))
-        {
-            this.ghostSR.enabled = true;
-            currFurniture.possessed = false;
-            currFurniture = null;
-        }
     }
 
     private void move()
@@ -41,16 +26,12 @@ public class Ghost : MonoBehaviour
         {
             currFurniture.furnitureRB.velocity = new Vector2(x_input, y_input).normalized * currFurniture.speed;
         }
-        //Debug.Log(gm.calculateScore());
+        Debug.Log(gm.calculateScore());
     }
 
     public void possess(Furniture f)
     {
-        this.ghostSR.enabled = false;
-        if(currFurniture)
-        {
-            currFurniture.possessed = false;
-        }
+        currFurniture.possessed = false;
         currFurniture = f;
         f.possessed = true;
     }
