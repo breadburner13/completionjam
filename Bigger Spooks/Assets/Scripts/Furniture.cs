4924f0ec75af80b4d734eccb4affae92d921ef72
BreadBurner13 <jungkrischan@gmail.com>
2020-12-27 18:52:20 -0800

    add basic ghost and furniture movement

---
+++ Bigger Spooks/Assets/Scripts/Furniture.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Furniture : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool possessed; 
+    public float speed;
+    public Rigidbody2D furnitureRB;
+    void Awake() 
+    {
+        furnitureRB = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        
+    }
+}
---
+++ Bigger Spooks/Assets/Scripts/Ghost.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public Furniture currFurniture;
+    private float x_input;
+    private float y_input;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        x_input = Input.GetAxisRaw("Horizontal");
+        y_input = Input.GetAxisRaw("Vertical");
+        move();
+    }
+
+    private void move()
+    {
+        if(currFurniture)
+        {
+            currFurniture.furnitureRB.velocity = new Vector2(x_input, y_input).normalized * currFurniture.speed;
+        }
+    }
+}
