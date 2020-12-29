838f2f9cb634a482c9406c3a81867a254c137e48
kevponce4 <kev.ponce4@gmail.com>
2020-12-28 20:45:45 -0800

    investagator done

--- Bigger Spooks/Assets/Scripts/Investigator.cs
+++ Bigger Spooks/Assets/Scripts/Investigator.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Investigator : MonoBehaviour
 {
     #region movment_vars
     private Rigidbody2D InvestRB;
+    [SerializeField]
     private float move_speed;
     [SerializeField]
     private FurnitureManager Fmanager;
@@ -15,6 +17,7 @@ public class Investigator : MonoBehaviour
     private bool start_exit;
     private bool exiting;
     private bool entering;
+    [SerializeField]
     private GameObject door;
     #endregion
 
@@ -23,6 +26,8 @@ public class Investigator : MonoBehaviour
     [SerializeField]
     private float attack_length;
     private bool attacking;
+    [SerializeField]
+    private Image flash;
     #endregion
 
     #region Unity_funcs
@@ -58,47 +63,86 @@ public class Investigator : MonoBehaviour
     #region Move_funcs
     private void move()
     {
-        Vector3 target = Fmanager.tracking.position;
-        Vector2 direction = target - transform.position;
-        InvestRB.velocity = direction.normalized * move_speed;
+        if (Fmanager.tracking.transform.CompareTag("Furniture"))
+        {
+            Vector3 target = Fmanager.tracking.position;
+            Vector2 direction = target - transform.position;
+            InvestRB.velocity = direction.normalized * move_speed;
+        }
+        else
+        {
+            InvestRB.velocity = Vector2.zero;
+        }
     }
     private void exit()
     {
-        if(start_exit && Vector3.Distance(door.transform.position, transform.position) > 1)
+        if (Vector3.Distance(door.transform.position, transform.position) <= .5)
+        {
+            start_exit = false;
+        }
+
+        if (start_exit && Vector3.Distance(door.transform.position, transform.position) > .5)
         {
             Vector2 direction =  door.transform.position - transform.position;
             InvestRB.velocity = direction.normalized * move_speed;
-            if(Vector3.Distance(door.transform.position, transform.position) <= 1)
-            {
-                start_exit = false;
-            }
+            return;
         }
-   
-        if(transform.position == door.transform.position - door_vect)
+
+        if(Vector3.Distance(transform.position, door.transform.position - door_vect) < .5 && !start_exit)
         {
             exiting = false;
+            entering = true;
         }
+        
         InvestRB.velocity = -(door_vect.normalized) * move_speed;
-        entering = true;
-
     }
 
     private void enter()
     {
-        if (transform.position == door.transform.position + door_vect)
+        if (Vector3.Distance(transform.position, door.transform.position + door_vect) < 1)
         {
             entering = false;
+            attacking = false;
         }
         InvestRB.velocity = door_vect.normalized * move_speed;
-        entering = false;
     }
     #endregion
 
     #region attack_func
     IEnumerator attack()
     {
+        Debug.Log("attacked");
+        attacking = true;
+        attack_timer = attack_length;
+        flash.color = new Color(flash.color.r, flash.color.g, flash.color.b, 255f);
+        InvestRB.velocity = Vector2.zero;
         start_exit = true;
+        exiting = true;
+        float alpha = flash.color.a;
+        float timer = 0;
+        while(alpha > 0)
+        {
+            Debug.Log("flashing");
+
+            flash.color = Color.Lerp(flash.color, new Color(flash.color.r, flash.color.g, flash.color.b, 0), timer/15);
+            alpha = flash.color.a;
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        Debug.Log("no flash");
         yield return null;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!attacking && collision.transform.CompareTag("Furniture"))
+        {
+            Furniture fur = collision.transform.GetComponent<Furniture>();
+            if (fur.possessed)
+            {
+                StartCoroutine(attack());
+            }
+        }
     }
     #endregion
 }
