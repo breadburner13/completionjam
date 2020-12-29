ff0d3eea22e3b1c583ac9b77c8d4d34b693ae5e8
kevponce4 <kev.ponce4@gmail.com>
2020-12-28 16:27:04 -0800

    Investagtor move update

--- Bigger Spooks/Assets/Scripts/Investigator.cs
+++ Bigger Spooks/Assets/Scripts/Investigator.cs
@@ -10,15 +10,18 @@ public class Investigator : MonoBehaviour
     [SerializeField]
     private FurnitureManager Fmanager;
     [SerializeField]
-    private Vector2 door_vect;
+    [Tooltip("the direction the investigator enters from")]
+    private Vector3 door_vect;
+    private bool start_exit;
+    private bool exiting;
+    private bool entering;
+    private GameObject door;
     #endregion
 
     #region attack_vars
     private float attack_timer;
     [SerializeField]
     private float attack_length;
-    private bool exiting;
-    private bool entering;
     private bool attacking;
     #endregion
 
@@ -37,12 +40,16 @@ public class Investigator : MonoBehaviour
         {
             move();
         }
+        else if (exiting)
+        {
+            exit();
+        }
         else if(attack_timer > 0)
         {
             attack_timer -= Time.deltaTime;
-        } else if (exiting)
+        } else if (entering)
         {
-            exit();
+            enter();
         }
         
     }
@@ -57,12 +64,41 @@ public class Investigator : MonoBehaviour
     }
     private void exit()
     {
+        if(start_exit && Vector3.Distance(door.transform.position, transform.position) > 1)
+        {
+            Vector2 direction =  door.transform.position - transform.position;
+            InvestRB.velocity = direction.normalized * move_speed;
+            if(Vector3.Distance(door.transform.position, transform.position) <= 1)
+            {
+                start_exit = false;
+            }
+        }
+   
+        if(transform.position == door.transform.position - door_vect)
+        {
+            exiting = false;
+        }
+        InvestRB.velocity = -(door_vect.normalized) * move_speed;
+        entering = true;
 
     }
 
     private void enter()
     {
+        if (transform.position == door.transform.position + door_vect)
+        {
+            entering = false;
+        }
+        InvestRB.velocity = door_vect.normalized * move_speed;
+        entering = false;
+    }
+    #endregion
 
+    #region attack_func
+    IEnumerator attack()
+    {
+        start_exit = true;
+        yield return null;
     }
     #endregion
 }
