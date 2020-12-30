fded8dcc12b23acb6615ecd5abb4fae1d14ee0a0
amiyourtype <35315317+amiyourtype@users.noreply.github.com>
2020-12-30 03:51:03 -0800

    lore

---
+++ Bigger Spooks/Assets/Scripts/slideshow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class slideshow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<string> slides;
+    public int index;
+    public string nextScene;
+    public Text text;
+
+    void Start()
+    {
+        index = 0;
+        text.text = slides[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonUp(0))
+        {
+            index++;
+            if(index == slides.Count)
+            {
+                SceneManager.LoadScene(nextScene);
+            } 
+            text.text = slides[index];
+        }
+    }
+}
