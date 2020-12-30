65b2cf55a7d261cc0270a3a6a2586c7277fc8172
amiyourtype <35315317+amiyourtype@users.noreply.github.com>
2020-12-30 02:14:07 -0800

    here comes the cavalry

--- Bigger Spooks/Assets/Scripts/ghostbuster.cs
+++ Bigger Spooks/Assets/Scripts/ghostbuster.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 
 public class GhostBuster : MonoBehaviour
 {
+    public bool possessed;
+    private Ghost ghost;
+
     #region movment vars
     [SerializeField]
-    private float speed;
+    public float speed;
     public float distance;
 
     [SerializeField] 
     private FurnitureManager fm;
-    private Rigidbody2D gbRB;
+    public Rigidbody2D gbRB;
     #endregion
 
     public Health health;
@@ -32,6 +35,7 @@ public class GhostBuster : MonoBehaviour
     {
         //get the transform of the ghost
         gbRB = GetComponent<Rigidbody2D>();
+        ghost = FindObjectOfType<Ghost>();
         attack_timer = 0;
         fm = FindObjectOfType<FurnitureManager>();
         laser = GetComponent<LaserController>();
@@ -83,4 +87,15 @@ public class GhostBuster : MonoBehaviour
         }
     }
     
+    void OnMouseDown() 
+    {
+        // Debug.Log("Kimi wa ne tashika ni ano toki watashi no soba ni ita Itsudatte itsudatte itsudatte sugu yoko de waratteita nakushitemo"); 
+        // Debug.Log("Torimodosu kimi wo I will never leave you. If you wanna battle, then Ill take it to the streets Where theres no rules Take off the gloves ref,"); 
+        // Debug.Log("please step down Gotta prove my skills so get down My lyrical dempsey roll about to smack down now Gotta shoot to kill and shoot the skill Dont you be afraid,");
+        // Debug.Log("mans gotta do how it feels Six to seven to eight to nine ten I flip the script to make it to the top ten, go Dreamless dorm, ticking clock I walk away from the");
+        // Debug.Log("soundless room Windless night, moonlight melts My ghostly shadow into the lukewarm gloom Nightly dance of bleeding swords Reminds me that I still live Every mans ");
+        // Debug.Log("gotta fight the fear Im the first to admit it Sheer thoughts provoke the new era Become a big terror, but my only rival is my shadow Rewind then play it back and");
+        // Debug.Log("fix my own error Get low to the ground, its getting better Like I told you before, double up and take more cheddar L to the J, say stay laced, heres my card, B Royal flush and Im the ace");
+        ghost.possessGB(this);
+    }
 }
